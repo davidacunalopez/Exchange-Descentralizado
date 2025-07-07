066e22bd4bf2f32e147854353fa58ed884b50db1
David Acuña López <89756491+davidacunalopez@users.noreply.github.com>
2025-07-07 13:22:34 -0600

    Add files via upload

---
+++ SimpleDEX.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SimpleDEX {
+    // Variables
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+    address public owner;
+
+    uint256 public reserveA;
+    uint256 public reserveB;
+
+    // Eventos
+    event LiquidityAdded(uint256 amountA, uint256 amountB);
+    event LiquidityRemoved(uint256 amountA, uint256 amountB);
+    event Swapped(address indexed user, address inputToken, uint256 inputAmount, address outputToken, uint256 outputAmount);
+
+    // Constructor
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+        owner = msg.sender;
+    }
+
+    // Modificador
+    modifier onlyOwner() {
+        require(msg.sender == owner, "No eres el propietario");
+        _;
+    }
+
+    // Funciones
+    function addLiquidity(uint256 amountA, uint256 amountB) external onlyOwner {
+        require(tokenA.transferFrom(msg.sender, address(this), amountA), "TokenA transfer failed");
+        require(tokenB.transferFrom(msg.sender, address(this), amountB), "TokenB transfer failed");
+        reserveA += amountA;
+        reserveB += amountB;
+        emit LiquidityAdded(amountA, amountB);
+    }
+
+    function swapAforB(uint256 amountAIn) external {
+        require(tokenA.transferFrom(msg.sender, address(this), amountAIn), "TokenA transfer failed");
+
+        uint256 amountBOut = getSwapAmount(amountAIn, reserveA, reserveB);
+        require(tokenB.transfer(msg.sender, amountBOut), "TokenB transfer failed");
+
+        reserveA += amountAIn;
+        reserveB -= amountBOut;
+
+        emit Swapped(msg.sender, address(tokenA), amountAIn, address(tokenB), amountBOut);
+    }
+
+    function swapBforA(uint256 amountBIn) external {
+        require(tokenB.transferFrom(msg.sender, address(this), amountBIn), "TokenB transfer failed");
+
+        uint256 amountAOut = getSwapAmount(amountBIn, reserveB, reserveA);
+        require(tokenA.transfer(msg.sender, amountAOut), "TokenA transfer failed");
+
+        reserveB += amountBIn;
+        reserveA -= amountAOut;
+
+        emit Swapped(msg.sender, address(tokenB), amountBIn, address(tokenA), amountAOut);
+    }
+
+    function removeLiquidity(uint256 amountA, uint256 amountB) external onlyOwner {
+        require(amountA <= reserveA && amountB <= reserveB, "Insufficient liquidity");
+
+        reserveA -= amountA;
+        reserveB -= amountB;
+
+        require(tokenA.transfer(msg.sender, amountA), "TokenA transfer failed");
+        require(tokenB.transfer(msg.sender, amountB), "TokenB transfer failed");
+
+        emit LiquidityRemoved(amountA, amountB);
+    }
+
+    function getPrice(address _token) external view returns (uint256) {
+        if (_token == address(tokenA)) {
+            return (reserveB * 1e18) / reserveA;
+        } else if (_token == address(tokenB)) {
+            return (reserveA * 1e18) / reserveB;
+        } else {
+            revert("Invalid token");
+        }
+    }
+
+    function getSwapAmount(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) internal pure returns (uint256) {
+        // fórmula sin fee: (y * dx) / (x + dx)
+        return (outputReserve * inputAmount) / (inputReserve + inputAmount);
+    }
+}
