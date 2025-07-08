40fe6710cad88fa10c92430626875b107951fbe7
David Acuña López <89756491+davidacunalopez@users.noreply.github.com>
2025-07-07 21:43:37 -0600

    Update TokenB.sol

--- TokenB.sol
+++ TokenB.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// Este es un contrato similar al ejemplo dado
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenB is ERC20 {
