bf9efd36f023bc3545cac83f4002be83c20c0598
David Acuña López <89756491+davidacunalopez@users.noreply.github.com>
2025-07-07 21:45:21 -0600

    Update SimpleDEX.sol

--- SimpleDEX.sol
+++ SimpleDEX.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+// Este es un contrato que representara mi exchange
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SimpleDEX {
